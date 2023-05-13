9d3a74534649024b4b4c4a66b75f311285d090ca
Grace O'Leary <goleary@udel.edu>
2023-05-13 16:13:13 -0400

    Solved Graph

--- _bfs.py
+++ _bfs.py
@@ -22,53 +22,56 @@ class Bfs:
         ]
         
         edges= [
-            ("A" , "B", {"distance": random.randint(0,10)}), 
-            ("A" , "D", {"distance": random.randint(0,10)}),
-            ("B" , "E", {"distance": random.randint(0,10)}),
-            ("B" , "C", {"distance": random.randint(0,10)}),
-            ("D" , "F", {"distance": random.randint(0,10)}),
-            ("D" , "G", {"distance": random.randint(0,10)}),
-            ("E" , "G", {"distance": random.randint(0,10)}),
-            ("E" , "D", {"distance": random.randint(0,10)}),
-            ("E" , "H", {"distance": random.randint(0,10)}),
-            ("H" , "M", {"distance": random.randint(0,10)}),
-            ("C" , "I", {"distance": random.randint(0,10)}),
-            ("C" , "J", {"distance": random.randint(0,10)}),
-            ("A" , "K", {"distance": random.randint(0,10)}),
-            ("K" , "L", {"distance": random.randint(0,10)}),
-            ("L" , "J", {"distance": random.randint(0,10)}),
-            ("I" , "M", {"distance": random.randint(0,10)}),
-            ("M" , "G", {"distance": random.randint(0,10)}),
-            ("J" , "N", {"distance": random.randint(0,10)}),
-            ("N" , "O", {"distance": random.randint(0,10)}),
-            ("M" , "P", {"distance": random.randint(0,10)}),
-            ("P" , "O", {"distance": random.randint(0,10)}),
-            ("O" , "K", {"distance": random.randint(0,10)}),
-            ("O" , "Q", {"distance": random.randint(0,10)}),
-            ("F" , "G", {"distance": random.randint(0,10)}),
-            ("P" , "I", {"distance": random.randint(0,10)}),
-            ("J" , "Q", {"distance": random.randint(0,10)}),
-            ("G" , "R", {"distance": random.randint(0,10)}),
-            ("R" , "S", {"distance": random.randint(0,10)}),
-            ("O" , "S", {"distance": random.randint(0,10)}),
-            ("S" , "T", {"distance": random.randint(0,10)}),
-            ("T" , "R", {"distance": random.randint(0,10)}),
-            ("R" , "U", {"distance": random.randint(0,10)}),
-            ("U" , "V", {"distance": random.randint(0,10)}),
-            ("G" , "W", {"distance": random.randint(0,10)}),
-            ("W" , "X", {"distance": random.randint(0,10)}),
-            ("X" , "V", {"distance": random.randint(0,10)}),
-            ("X" , "R", {"distance": random.randint(0,10)})  
+            ("A" , "B", {"distance": random.randint(1,10)}), 
+            ("A" , "D", {"distance": random.randint(1,10)}),
+            ("B" , "E", {"distance": random.randint(1,10)}),
+            ("B" , "C", {"distance": random.randint(1,10)}),
+            ("D" , "F", {"distance": random.randint(1,10)}),
+            ("D" , "G", {"distance": random.randint(1,10)}),
+            ("E" , "G", {"distance": random.randint(1,10)}),
+            ("E" , "D", {"distance": random.randint(1,10)}),
+            ("E" , "H", {"distance": random.randint(1,10)}),
+            ("H" , "M", {"distance": random.randint(1,10)}),
+            ("C" , "I", {"distance": random.randint(1,10)}),
+            ("C" , "J", {"distance": random.randint(1,10)}),
+            ("A" , "K", {"distance": random.randint(1,10)}),
+            ("K" , "L", {"distance": random.randint(1,10)}),
+            ("L" , "J", {"distance": random.randint(1,10)}),
+            ("I" , "M", {"distance": random.randint(1,10)}),
+            ("M" , "G", {"distance": random.randint(1,10)}),
+            ("J" , "N", {"distance": random.randint(1,10)}),
+            ("N" , "O", {"distance": random.randint(1,10)}),
+            ("M" , "P", {"distance": random.randint(1,10)}),
+            ("P" , "O", {"distance": random.randint(1,10)}),
+            ("O" , "K", {"distance": random.randint(1,10)}),
+            ("O" , "Q", {"distance": random.randint(1,10)}),
+            ("F" , "G", {"distance": random.randint(1,10)}),
+            ("P" , "I", {"distance": random.randint(1,10)}),
+            ("J" , "Q", {"distance": random.randint(1,10)}),
+            ("G" , "R", {"distance": random.randint(1,10)}),
+            ("R" , "S", {"distance": random.randint(1,10)}),
+            ("O" , "S", {"distance": random.randint(1,10)}),
+            ("S" , "T", {"distance": random.randint(1,10)}),
+            ("T" , "R", {"distance": random.randint(1,10)}),
+            ("R" , "U", {"distance": random.randint(1,10)}),
+            ("U" , "V", {"distance": random.randint(1,10)}),
+            ("G" , "W", {"distance": random.randint(1,10)}),
+            ("W" , "X", {"distance": random.randint(1,10)}),
+            ("X" , "V", {"distance": random.randint(1,10)}),
+            ("X" , "R", {"distance": random.randint(1,10)})  
         ]
 
         # add all the food trucks to the graph
         self.foodTruckMap.add_nodes_from(foodTrucks)
         self.foodTruckMap.add_edges_from(edges)
 
-        #compute the shortest path stating at A to the food trucks, using bsf
-        shortest = nx.bfs_tree(self.foodTruckMap, foodTrucks[0], False, len(self.foodTruckMap))
+        #graph visual
+        bfsGraph = '/lesson-19-graph-applications/graphviz.svg'
 
-       #Draw graph
+        #solution code
+        #sorted = nx.all_shortest_paths(self.foodTruckmap)
+        solution = [p for p in nx.shortest_path(self.foodTruckMap, source='A', target='X')]
+        print(solution)
 
         """ def shortestPath(self):
         
