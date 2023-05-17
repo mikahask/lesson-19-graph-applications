f2c62a7c5960fac3e6690bb0daa558b99a50b502
Mikaylla <mikahas@udel.edu>
2023-05-16 21:05:10 -0400

    updating graph

--- test3.py
+++ test3.py
@@ -1,42 +1,87 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-def is_bipartite(graph):
-    """
-    Returns True if the given graph is bipartite, and False otherwise.
-    """
-    # Start DFS from an arbitrary node in the graph
-    start_node = next(iter(graph.nodes()))
-    visited = {start_node: 0}  # mark the start node as visited and assign it to set 0
-    stack = [start_node]
-
-    # Perform DFS while maintaining the visited set and the set assignments
-    while stack:
-        node = stack.pop()
-        node_set = visited[node]
-
-        # Assign the opposite color to all unvisited neighbors
-        for neighbor in graph.neighbors(node):
-            if neighbor not in visited:
-                visited[neighbor] = 1 - node_set
-                stack.append(neighbor)
-            elif visited[neighbor] == node_set:
-                # If a neighbor has the same color as the current node, the graph is not bipartite
-                return False
-
-    # If we've visited all nodes without conflicts, the graph is bipartite
-    return True
-
 G = nx.Graph()
-G.add_nodes_from(range(20))
-for i in range(20):
-    for j in range(i+1, 20):
-        G.add_edge(i, j)
 
-pos = {i: (i % 10, i // 10) if i < 10 else ((i-10) % 10, (i-10) // 10 + 1) for i in range(20)}
+# Add nodes with labels A to V
+for i in range(65, 87):
+    G.add_node('' + chr(i))
 
-nx.draw(G, pos=pos, with_labels=True)    
-plt.show() # show the plot
+# Add edges to the graph
+G.add_edge('A', 'F', weight=7)
+G.add_edge('A', 'D', weight=5)
+G.add_edge('B', 'C', weight=8)
+G.add_edge('B', 'F', weight=9)
+G.add_edge('B', 'E', weight=7)
+G.add_edge('C', 'G', weight=5)
+G.add_edge('D', 'F', weight=6)
+G.add_edge('D', 'E', weight=15)
+G.add_edge('E', 'F', weight=8)
+G.add_edge('E', 'J', weight=9)
+G.add_edge('F', 'G', weight=11)
+G.add_edge('F', 'H', weight=8)
+G.add_edge('G', 'I', weight=9)
+G.add_edge('H', 'I', weight=7)
+G.add_edge('H', 'L', weight=5)
+G.add_edge('I', 'J', weight=6)
+G.add_edge('I', 'L', weight=12)
+G.add_edge('J', 'K', weight=10)
+G.add_edge('K', 'L', weight=6)
+G.add_edge('L', 'M', weight=8)
+G.add_edge('M', 'N', weight=9)
+G.add_edge('M', 'O', weight=10)
+G.add_edge('N', 'P', weight=6)
+G.add_edge('O', 'P', weight=7)
+G.add_edge('O', 'S', weight=5)
+G.add_edge('P', 'Q', weight=4)
+G.add_edge('Q', 'R', weight=6)
+G.add_edge('R', 'V', weight=9)
+G.add_edge('S', 'T', weight=5)
+G.add_edge('T', 'U', weight=7)
+G.add_edge('U', 'V', weight=8)
+G.add_edge('B', 'D', weight=3)
+G.add_edge('C', 'F', weight=7)
+G.add_edge('G', 'H', weight=5)
+G.add_edge('I', 'K', weight=10)
+G.add_edge('J', 'L', weight=4)
+G.add_edge('M', 'R', weight=12)
+G.add_edge('N', 'S', weight=8)
+G.add_edge('O', 'T', weight=6)
+G.add_edge('P', 'U', weight=9)
+G.add_edge('Q', 'V', weight=11)
+G.add_edge('L', 'S', weight=7)
+G.add_edge('L', 'V', weight=10)
+G.add_edge('L', 'A', weight=8)
+G.add_edge('L', 'B', weight=9)
+G.add_edge('A', 'G', weight=6)
+G.add_edge('B', 'D', weight=10)
+G.add_edge('C', 'H', weight=7)
+G.add_edge('D', 'J', weight=8)
+G.add_edge('E', 'K', weight=12)
+G.add_edge('F', 'M', weight=13)
+G.add_edge('G', 'N', weight=9)
+G.add_edge('H', 'O', weight=11)
+G.add_edge('I', 'P', weight=7)
+G.add_edge('J', 'Q', weight=5)
+G.add_edge('K', 'R', weight=9)
+G.add_edge('L', 'S', weight=7)
+G.add_edge('M', 'T', weight=6)
+G.add_edge('N', 'U', weight=8)
+G.add_edge('O', 'V', weight=10)
 
-# Check if the graph is bipartite
-print(is_bipartite(G))
+# Define edge labels
+edge_labels = nx.get_edge_attributes(G, 'weight')
+T = nx.algorithms.tree.minimum_spanning_tree(G.subgraph(nx.node_connected_component(G, 'A')), weight='weight')
+
+# Draw the graph and tree
+pos = nx.spring_layout(G)
+nx.draw(G, pos, with_labels=True)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+nx.draw_networkx_edges(G, pos, edge_color='yellow', width=2)
+plt.show()
+pos = nx.spring_layout(G)
+nx.draw_networkx_nodes(G, pos, node_color='lightblue')
+nx.draw_networkx_labels(G, pos)
+nx.draw_networkx_edges(G, pos)
+nx.draw_networkx_edges(T, pos, edge_color='red', width=2)
+plt.show()
